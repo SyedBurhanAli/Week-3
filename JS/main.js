36211947a59786ddc2fd119af168dd505e32be00
Syed Burhan Ali Shah <iburhan2018@gmail.com>
2020-10-06 19:45:02 +0500

    Function and Html from StakOverFlow

--- JS/main.js
+++ JS/main.js
@@ -11,3 +11,10 @@ function tryNow() {
   }
   document.getElementById("demo").innerHTML = choice;
 }
+
+function myFunction() {
+  var age, voteable;
+  age = document.getElementById("age").value;
+  voteable = age < 18 ? "Too young" : "Old enough";
+  document.getElementById("dem").innerHTML = voteable;
+} //Took this from StackOverFlow
