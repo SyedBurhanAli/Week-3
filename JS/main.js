dff75f810e27a113be6282ec01990a2c111bf291
Syed Burhan Ali Shah <iburhan2018@gmail.com>
2020-10-06 20:48:00 +0500

    Change Div Background Color with Buttons

--- JS/main.js
+++ JS/main.js
@@ -18,3 +18,15 @@ function myFunction() {
   voteable = age < 18 ? "Too young" : "Old enough";
   document.getElementById("dem").innerHTML = voteable;
 } //Took this from StackOverFlow
+
+function changecolor() {
+  //Getting HTML Element
+  var dd1 = document.getElementById("d1");
+  var dd2 = document.getElementById("d2");
+  /* "document" refers to all the html webpage getElementById("d1") is a method goes into document having Parameter/Id "d1". */
+
+  //Setting the CSS Class
+  dd1.className = "blueback";
+  dd2.className = "yellowback";
+  //As the name suggest its xyz.className so inside quotation marks we give Class-Name
+}
